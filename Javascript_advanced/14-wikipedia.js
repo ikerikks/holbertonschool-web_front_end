1d2f04ebc141eb7213ab99641572f51f5cc79888
ikerikks <ikerikks@gmail.com>
2023-09-21 15:29:40 +0200

    update

--- Javascript_advanced/14-wikipedia.js
+++ Javascript_advanced/14-wikipedia.js
@@ -1,32 +1,10 @@
-// function createElement(data) {
-//   let paragraph = document.createElement("p");
-//   paragraph.innerText = data;
-//   document.body.append(paragraph);
-// }
 
-// function queryWikipedia(callback) {
-//   let url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=";
-//   let xhr = new XMLHttpRequest();
-//   xhr.open("GET", url);
-//   xhr.responseType = "json";
-//   // callback(xhr.response);
-// }
-
-// createElement('my boy');
-
-// queryWikipedia(createElement);
-// // createElement('my boy');
-
-// --------------------------------
-
-// Function to create a paragraph element with given data and append it to the document body
 function createElement(data) {
   var paragraph = document.createElement('p');
   paragraph.textContent = data;
   document.body.appendChild(paragraph);
 }
 
-// Function to fetch data from Wikipedia and call the callback function with the response
 function queryWikipedia(callback) {
   // Define the Wikipedia API URL
   var apiUrl = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*';
@@ -61,5 +39,4 @@ function queryWikipedia(callback) {
   xhr.send();
 }
 
-// Call queryWikipedia with createElement as the callback
 queryWikipedia(createElement);
